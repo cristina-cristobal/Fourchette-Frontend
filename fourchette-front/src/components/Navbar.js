35bdb9bd7df11968bc45542ecd801deaef2d4181
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-10 14:58:41 -0400

    added logo

--- fourchette-front/src/components/Navbar.js
+++ fourchette-front/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-// import logo_transparent.png from '../imgs/logo_transparent'
+import logo from '../imgs/logo_transparent.png'
 
 const Navbar = (props) => {
   return(
@@ -8,6 +8,7 @@ const Navbar = (props) => {
      <Link to="/" className="item">
          <div className="content">Fourchette</div>
      </Link>
+    { <img src={logo} className="logo"/> }
      <Link to="/" className="item">Home</Link>
      <Link to="/profile" className="item">Profile</Link>
    </div>
