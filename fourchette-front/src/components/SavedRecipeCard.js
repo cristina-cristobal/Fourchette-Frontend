bf6ad3e9083a519d21a6638757becb984a50307b
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-11 13:21:35 -0400

    fixed saved recipe link

--- fourchette-front/src/components/SavedRecipeCard.js
+++ fourchette-front/src/components/SavedRecipeCard.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const SavedRecipeCard = (props) => {
   // console.log(props)
   return (
-    <Link to={`/recipes/${props.recipe.id}`}>
+    <Link to={`/recipes/${props.recipe.recipe.id}`}>
     <div>
       <img className="cardimg" src={props.recipe.recipe.image}/>
       <div className="recipe-info">
