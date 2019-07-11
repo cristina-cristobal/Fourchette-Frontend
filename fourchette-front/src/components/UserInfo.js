8410d2dae9dd5d61160b69d0371d688d0863c6c0
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-11 10:17:44 -0400

    tweaked profile page

--- fourchette-front/src/components/UserInfo.js
+++ fourchette-front/src/components/UserInfo.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react'
+import profile from '../imgs/prof.png'
+
 
 export default class UserInfo extends Component {
   render(){
     return(
-      <div>
-        User Info: Image and Name
+      <div className="user-info profile-column">
+        {<img className="profile-icon" src={profile} />}
+        <div className="user-font">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        Tina Cristobal
+        </div>
       </div>
     )
   }
