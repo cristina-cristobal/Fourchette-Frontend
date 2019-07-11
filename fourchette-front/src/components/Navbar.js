dbf37dc9a91da5ee2831969f20c1b967cf8bc1df
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-11 10:31:54 -0400

    changed profile icon

--- fourchette-front/src/components/Navbar.js
+++ fourchette-front/src/components/Navbar.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../imgs/logo_transparent.png'
 import fourchette from '../imgs/fourchette.png'
+import profile from '../imgs/profile.png'
+
 
 const Navbar = (props) => {
   return(
     <div className="ui inverted olive menu navbar">
      <Link to="/" className="item"><b>Home</b></Link>
-     <Link to="/profile" className="item"><b>Profile</b></Link>
+     <Link to="/profile" className="item">
+     {<img className="profile-small" src={profile} />}
+     </Link>
      {<img  src={fourchette} className="logo center"/>}
    </div>
   )
